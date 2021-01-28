7316d6b908372928642b9727fbc040fa3537fccd
forestfuture <morimiraimirai2013@gmail.com>
2021-01-28 16:46:50 +0900

    mobile対応

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 document.querySelector(".img_select1-1").onclick = function(){
-    document.querySelector(".img1-1").classList.remove("d-none");
     document.querySelector(".img1-2").classList.add("d-none");
     document.querySelector(".img1-3").classList.add("d-none");
+    document.querySelector(".img1-1").classList.remove("d-none");
     document.querySelector(".img_select1-1").classList.add("img_border");
     document.querySelector(".img_select1-2").classList.remove("img_border");
     document.querySelector(".img_select1-3").classList.remove("img_border");
 };
+
 document.querySelector(".img_select1-2").onclick = function(){
     document.querySelector(".img1-1").classList.add("d-none");
     document.querySelector(".img1-2").classList.remove("d-none");
@@ -14,6 +15,7 @@ document.querySelector(".img_select1-2").onclick = function(){
     document.querySelector(".img_select1-2").classList.add("img_border");
     document.querySelector(".img_select1-3").classList.remove("img_border");
 };
+
 document.querySelector(".img_select1-3").onclick = function(){
     document.querySelector(".img1-1").classList.add("d-none");
     document.querySelector(".img1-2").classList.add("d-none");
@@ -30,6 +32,7 @@ document.querySelector(".img_select2-1").onclick = function(){
     document.querySelector(".img_select2-2").classList.remove("img_border");
 
 };
+
 document.querySelector(".img_select2-2").onclick = function(){
     document.querySelector(".img2-1").classList.add("d-none");
     document.querySelector(".img2-2").classList.remove("d-none");
@@ -45,6 +48,7 @@ document.querySelector(".img_select3-1").onclick = function(){
     document.querySelector(".img_select3-2").classList.remove("img_border");
     document.querySelector(".img_select3-3").classList.remove("img_border");
 };
+
 document.querySelector(".img_select3-2").onclick = function(){
     document.querySelector(".img3-1").classList.add("d-none");
     document.querySelector(".img3-2").classList.remove("d-none");
@@ -53,6 +57,7 @@ document.querySelector(".img_select3-2").onclick = function(){
     document.querySelector(".img_select3-2").classList.add("img_border");
     document.querySelector(".img_select3-3").classList.remove("img_border");
 };
+
 document.querySelector(".img_select3-3").onclick = function(){
     document.querySelector(".img3-1").classList.add("d-none");
     document.querySelector(".img3-2").classList.add("d-none");
